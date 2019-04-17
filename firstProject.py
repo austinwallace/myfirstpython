639e4918e805e017264757adf9bd06154f6d376b
admin <admin@admins-MacBook-Pro.local>
2019-04-17 07:47:16 -0400

    first python script

---
+++ firstProject.py
@@ -0,0 +1,12 @@
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+
+
+print ("Hello %s %s!" % (first_name, last_name))
+
+noun = input('Please provide a noun: ')
+verb = input('Please provide a verb: ')
+adjective = input('Please provide an adjective: ')
+noun2 = input('Please provide a second noun: ')
+
+print('%s %s into the %s %s' % (noun, verb, adjective, noun2))
